ff8a66b4beea2d7d182e1c593b29f793d9c1f614
JCBardellini <115193170+JCBardellini@users.noreply.github.com>
2023-09-04 22:02:28 -0700

    adding elements

--- index.js
+++ index.js
@@ -69,13 +69,150 @@ const genericMotivationQuotes = [
 
 // today's highlight btn 
 
+let isHighlightCreated = false;
+
+todayHighlightsBtn.addEventListener("click", () => {
+    if (!isHighlightCreated) {
+
+        const highlightsSection = document.createElement("section");
+        highlightsSection.id = "highlights-container";
+        //title
+        const highlightTitle = document.createElement("h3");
+        highlightTitle.textContent = "Today's Highlights";
+        highlightTitle.classList.add("title-highlights")
+
+        // air-quality div
+
+        const airQualityContainer = document.createElement("div");
+        airQualityContainer.id = "air-quality";
+        // air quality title 
+        const airQualityTitle = document.createElement("h4");
+        airQualityTitle.textContent = "Air Quality";
+        airQualityTitle.classList.add("h-title");
+        // new aq container 
+        const aqContainer = document.createElement("div");
+        aqContainer.classList.add("aq-container");
+        // aq rating container 
+        const aqRating = document.createElement("div");
+        aqRating.classList.add("aq-rating");
+        // creating the ul
+        const aqListUl = document.createElement("ul");
+        aqListUl.classList.add("ul-highlight");
+        // creating the li for the air quality icon
+        const aqRatingIcon = document.createElement("li");
+        aqRatingIcon.classList.add("icons");
+        // creating the icon img
+        const aqRatingIconImg = document.createElement("img");
+        aqRatingIconImg.src = "/weather-icons/wind.png";
+        aqRatingIconImg.alt = "";
+        // putting the icon inside the li
+        aqRatingIcon.appendChild(aqRatingIconImg);
+        // create the li for the rating info
+        const aqRatingInfo = document.createElement("li");
+        aqRatingInfo.classList.add("highlights-info", "rating");
+        aqRatingInfo.textContent = "Good";
+        // appending the rating info and rating icon into the aqListUl
+        aqListUl.appendChild(aqRatingIcon);
+        aqListUl.appendChild(aqRatingInfo);
+        // appending the aqListUl into the aqRating div 
+        aqRating.appendChild(aqListUl);
+        // create a new div called pRating that will hold the pollutants
+        const pRatingContainer = createElement("div");
+        pRatingContainer.classList.add('p-rating');
+        // creating the title and adding text to it
+        const pTitle = createElement("h5");
+        pTitle.classList.add("pollutants");
+        pTitle.textContent = "Pollutants";
+        // creating the ul for the different pollutants
+        const pListUl = createElement("ul");
+        pListUl.classList.add("ul-highlight p-info");
+        // adding the li to it
+        const pollutantO3 = document.createElement("li");
+        pollutantO3.classList.add("highlights-info");
+        pollutantO3.innerHTML = '<span class="color">O3:</span> 7.5';
+
+        const pollutantSO2 = document.createElement("li");
+        pollutantSO2.classList.add("highlights-info");
+        pollutantSO2.innerHTML = '<span class="color">SO2:</span> 42';
+
+        const pollutantNO2 = document.createElement("li");
+        pollutantNO2.classList.add("highlights-info");
+        pollutantNO2.innerHTML = '<span class="color">NO2:</span> 12';
+
+        const pollutantCO = document.createElement("li");
+        pollutantCO.classList.add("highlights-info");
+        pollutantCO.innerHTML = '<span class="color">CO:</span> 12';
+        // putting the pollutants to the UL
+        pListUl.appendChild(pollutantO3);
+        pListUl.appendChild(pollutantSO2);
+        pListUl.appendChild(pollutantNO2);
+        pListUl.appendChild(pollutantCO);
+        // now i have to put the ul and title inside the p rating container
+        pRatingContainer.appendChild(pTitle);
+        pRatingContainer.appendChild(pListUl);
+        // now i have to append the pRatingContainer in the aqContainer
+        aqContainer.appendChild(aqRating);
+        aqContainer.appendChild(pRatingContainer);
+        // append the aqContainer inside airQualityContainer
+        airQualityContainer.appendChild(aqContainer);
+
+        // sunrise & sunset div, will repeat the steps as above
+
+        const sunRiseSunSet = document.createElement("div");
+        sunRiseSunSet.id = "sunset-sunrise";
+
+        const sSTitle = document.createElement("h4");
+        sSTitle.classList.add("h-title");
+        sSTitle.textContent = "Sunrise & Sunset";
+
+        //sunrise container
+        const sunriseSunsetContainer = document.createElement("div");
+        sunriseSunsetContainer.classList.add("s-container");
+
+        const sunriseDiv = document.createElement("div");
+        sunriseDiv.classList.add("sunrise");
+
+        const sunriseTitle = document.createElement("h5");
+        sunriseTitle.textContent = "Sunrise"
+
+        const sunriseUlList = document.createElement("ul");
+        sunriseUlList.classList.add("ul-highlight");
+
+        const sunriseIcon = document.createElement("li");
+        sunriseIcon.classList.add("icons");
+
+        const sunriseIconImg = document.createElement("img");
+        sunriseIconImg.src = "/weather-icons/sunrise.png";
+        sunriseIconImg.alt = "";
+        sunriseIcon.appendChild(sunriseIconImg);
+
+        const sunriseInfo = document.createElement("li");
+        sunriseInfo.classList.add("highlights-info", "s-rise");
+        sunriseInfo.textContent = "6:32 AM";
+
+        sunriseUlList.appendChild(sunriseIcon);
+        sunriseUlList.appendChild(sunriseInfo);
+
+        sunriseDiv.appendChild(sunriseUlList);
+
+
+
+        //sunset container
+        
+
+        sunriseSunsetContainer.appendChild(sunriseDiv);
+        sunriseSunsetContainer.appendChild(sunriseDiv);
+        
+    }
+});
+
 // hourly btn
 const hourlyData = [
-    { time: "Now", icon: "/weather-icons/weather/clear-sky.png", temperature: "65&deg;" },
-    { time: "2 hours", icon: "/weather-icons/weather/rain.png", temperature: "68&deg;" },
-    { time: "4 hours", icon: "/weather-icons/weather/rainy.png", temperature: "55&deg;" },
-    { time: "6 hours", icon: "/weather-icons/weather/snow.png", temperature: "45&deg;" },
-    { time: "8 hours", icon: "/weather-icons/weather/sunny.png", temperature: "70&deg;" }
+    { time: "Now", icon: "/weather-icons/weather/clear-sky.png", temperature: "65°" },
+    { time: "2 hours", icon: "/weather-icons/weather/rain.png", temperature: "68°" },
+    { time: "4 hours", icon: "/weather-icons/weather/rainy.png", temperature: "55°" },
+    { time: "6 hours", icon: "/weather-icons/weather/snow.png", temperature: "45°" },
+    { time: "8 hours", icon: "/weather-icons/weather/sunny.png", temperature: "70°" }
 ];
 
 let isHourlyCreated = false;
@@ -99,14 +236,37 @@ hourlyBtn.addEventListener("click", () => {
             hourlyContainer.classList.add("h-container");
             // create the ul element
             const hourlyUl = document.createElement("ul");
-            hourlyUl.classList.add("time");
+            hourlyUl.classList.add("h-ul");
             // create the li elements 
             const timeLi = document.createElement("li");
             timeLi.classList.add("time");
-            timeLi.textContent = hourlyData.time;
-
-            
-        })
+            timeLi.textContent = dataObject.time;
+            // weather icon li
+            const hourlyWeatherLi = document.createElement("li");
+            const hourlyWeatherIconImg = document.createElement("img");
+            hourlyWeatherIconImg.src = dataObject.icon
+            hourlyWeatherIconImg.classList.add("weather");
+            hourlyWeatherLi.appendChild(hourlyWeatherIconImg);
+            // temp li
+            const hourlyTempLi = document.createElement("li");
+            hourlyTempLi.classList.add("deg");
+            hourlyTempLi.textContent = dataObject.temperature;
+            // put the li inside the Ul
+            hourlyUl.appendChild(timeLi)
+            hourlyUl.appendChild(hourlyWeatherIconImg)
+            hourlyUl.appendChild(hourlyTempLi)
+            // put them inside the the div container
+            hourlyContainer.appendChild(hourlyUl);
+            // append to parent
+            timeContainer.appendChild(hourlyContainer);
+        });
+    // put it inside the parent of the hourly container
+    hourlySection.appendChild(hourlyTitle);
+    hourlySection.appendChild(timeContainer);
+    // add to the page container
+    weatherInfoContainer.appendChild(hourlySection);
+    // set it to true 
+    isHourlyCreated = true;
     }
 })
 
@@ -114,14 +274,14 @@ hourlyBtn.addEventListener("click", () => {
 
 // need to create a forecast data that will get the day, weather type and temperature
 const forecastData = [
-    { day: "Now", icon: "/weather-icons/weather/clear-sky.png", temperature: "65&deg;" },
-    { day: "Monday", icon: "/weather-icons/weather/rain.png", temperature: "68&deg;" },
-    { day: "Tuesday", icon: "/weather-icons/weather/rainy.png", temperature: "55&deg;" },
-    { day: "Wednesday", icon: "/weather-icons/weather/snow.png", temperature: "45&deg;" },
-    { day: "Thursday", icon: "/weather-icons/weather/sunny.png", temperature: "70&deg;" },
-    { day: "Friday", icon: "/weather-icons/weather/sunny.png", temperature: "70&deg;" },
-    { day: "Saturday", icon: "/weather-icons/weather/sunny.png", temperature: "70&deg;" },
-    { day: "Sunday", icon: "/weather-icons/weather/sunny.png", temperature: "70&deg;" }
+    { day: "Now", icon: "/weather-icons/weather/clear-sky.png", temperature: "65°" },
+    { day: "Monday", icon: "/weather-icons/weather/rain.png", temperature: "68°" },
+    { day: "Tuesday", icon: "/weather-icons/weather/rainy.png", temperature: "55°" },
+    { day: "Wednesday", icon: "/weather-icons/weather/snow.png", temperature: "45°" },
+    { day: "Thursday", icon: "/weather-icons/weather/sunny.png", temperature: "70°" },
+    { day: "Friday", icon: "/weather-icons/weather/sunny.png", temperature: "70°" },
+    { day: "Saturday", icon: "/weather-icons/weather/sunny.png", temperature: "70°" },
+    { day: "Sunday", icon: "/weather-icons/weather/sunny.png", temperature: "70°" }
 ];
 
 let isForecastCreated = false;
